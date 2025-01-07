bbf6d4f6bb26c6271ad1419cc9baad7e65bdc707
Linuxph <87934843+Linuxph@users.noreply.github.com>
2025-01-08 00:46:45 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      '/api/v1':'http://localhost:3000'
-    }
-  },
+  // server:{
+  //   // proxy:{
+  //   //   '/api/v1':'http://localhost:3000'
+  //   // }
+  // },
   plugins: [react()],
 })
